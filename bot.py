04e3fe0b816e49c8681dbe7847854ddb30571676
Mithrandir <superplane39@gmail.com>
2017-08-13 14:06:08 +0300

    add !blame
    for plum's permission, see https://chat.stackexchange.com/transcript/message/39384206#39384206

--- bot.py
+++ bot.py
@@ -213,6 +213,9 @@ def on_message(message, client):
 
         if is_trusted_user and message.content.lower().strip() == "!flipcoin":
             flip_coin()
+            
+        if is_trusted_user and message.content.lower().strip() == "!blame":
+            blame()
 
         if is_trusted_user and message.content.lower().strip() == "!recall":
             recall()
@@ -267,8 +270,13 @@ def on_message(message, client):
         #traceback.print_exc()
         #print ""
 
-def flip_coin():
+def 
+flip_coin():
     room.send_message(random.choice(["Red", "Blue"]))
+    
+def 
+blame():
+    room.send_message(random.choice(["It's [Mithrandir](https://chat.stackexchange.com/users/133031)'s fault.", "It's [n_palum](https://chat.stackexchange.com/users/263999)'s fault."]))
 
 def change_host(msg):
     global imagehost
